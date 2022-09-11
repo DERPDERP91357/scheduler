f0c1210dbf8dd3188319ef51f611c051d1bfca94
Daniel Suen <danielsuen200791357@gmail.com>
2022-09-11 03:26:08 -0400

    added views to appointment

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,10 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerList from "components/InterviewerList";
 import InterviewerListItem from "components/InterviewerListItem";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -134,3 +138,12 @@ storiesOf("Button", module)
           onChange={action("setInterviewer")}
         />
       ));
+
+      storiesOf("Appointment", module)
+      .addParameters({
+        backgrounds: [{ name: "white", value: "#fff", default: true }]
+      })
+      .add("Appointment", () => <Appointment />)
+      .add("Appointment with Time", () => <Appointment time={"12pm"} />)
+      .add("Header", () => <Header time={"12pm"} />)
+      .add("Empty", () => <Empty onAdd={action("onAdd")} />)
