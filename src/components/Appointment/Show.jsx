56d8468d0df9028eeece4ce36c32da256822f8c9
Daniel Suen <danielsuen2007@yahoo.ca>
2022-09-11 09:40:03 -0400

    testing

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -22,7 +22,7 @@ export default function Empty(props){
         className="appointment__actions-button"
         src="images/trash.png"
         alt="Delete"
-        onClick={props.onDelete}
+        onClick={props.onDeletde}
       />
     </section>
   </section>
